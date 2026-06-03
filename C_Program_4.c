5ba48c33ca6ee90e11e46f2a9fa8bc95c9814106
smharini18 <smharini18@gmail.com>
2026-06-03 15:00:51 +0530

    Create C_Program_4.c

---
+++ C_Program_4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int x;
+    printf("enter a number:");
+    scanf("%d",&x);
+    if(x>50)
+      printf("sucess");
+    else
+      printf("failure");
+    
+    return 0;
+}
