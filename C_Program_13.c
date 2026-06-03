95be5fa97a0f3dd232f8e250c10a66cec505e9e5
smharini18 <smharini18@gmail.com>
2026-06-03 17:28:46 +0530

    Create C_Program_13.c

---
+++ C_Program_13.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int x,y,z;
+    printf("enter a number:");
+    scanf("%d",&x);
+    y=x%10;
+    z=(x/10)%10;
+    if(y==z)
+      printf("sucess");
+    else
+      printf("failure");
+    
+    return 0;
+}
